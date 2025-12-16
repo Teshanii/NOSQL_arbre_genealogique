c2f324a7bdac74ec0ae207dd7684e0379fd5f19f
Lina CHELLALA <chellalalina2@gmail.com>
2025-12-17 00:04:37 +0100

    Ajout crud.go, search.go, export.go, stats.go, tree.go

---
+++ operations/stats.go
@@ -0,0 +1,85 @@
+package operations
+
+import (
+	"Autriche/database"
+	"Autriche/models"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// Compter le nombre total de personnes
+func CountIndividuals() (int64, error) {
+	collection := database.IndividualsCollection()                          // la fonction pour acceder a la collection individu..
+	count, err := collection.CountDocuments(context.Background(), bson.M{}) // pour compter les documents ca veut dire les individus
+	return count, err                                                       // pas de filtre on recup tout le monde
+}
+
+// Compter le nombre de personnes par genre
+func CountByGender(gender string) (int64, error) {
+	collection := database.IndividualsCollection()
+	count, err := collection.CountDocuments(context.Background(), bson.M{"gender": gender})
+	return count, err
+}
+
+// Trouver les personnes sans date de naissance
+func GetIndividualsWithoutBirthDate() ([]models.Individual, error) { // on affiche en liste
+	collection := database.IndividualsCollection()
+	var results []models.Individual
+
+	cursor, err := collection.Find(context.Background(), bson.M{"birth_date": ""})
+	if err != nil {
+		return nil, err
+	}
+
+	err = cursor.All(context.Background(), &results) // recupere tous les resultats
+	return results, err
+}
+
+// Calculer l'âge moyen
+func GetAverageAge() (float64, error) {
+	collection := database.IndividualsCollection()
+	var results []models.Individual
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return 0, err
+	}
+
+	err = cursor.All(context.Background(), &results)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(results) == 0 { //donc le nbr de personnes
+		return 0, nil
+	}
+
+	total := 0 // on initialise a 0 total c pour le total dage et count c un compteur pour les personnes
+	count := 0
+	for _, person := range results { // ignorer les inex (_)
+		if person.BirthDate != "" {
+			age := 2025 - getYear(person.BirthDate)
+			total = total + age
+			count = count + 1
+		}
+	}
+
+	if count == 0 {
+		return 0, nil
+	}
+
+	return float64(total) / float64(count), nil // c pour pouvoir avoir les chiffres en virgule
+}
+
+// Extraire l'année d'une date "1985-03-15" → 1985
+func getYear(date string) int {
+	if len(date) < 4 { // psk si c moins de 4 caractere c que c bzr ya pas dannée
+		return 0
+	}
+	year := 0                // on initialise year a 0 pour la boucle
+	for i := 0; i < 4; i++ { // ca commmence par lindex 0
+		year = year*10 + int(date[i]-'0') // date [i] donc c le nombre a chaque case de la liste
+	}
+	return year
+}
---
+++ operations/tree.go
@@ -0,0 +1,70 @@
+package operations
+
+import (
+	"Autriche/database"
+	"Autriche/models"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// Trouver les parents d'une personne
+func GetParents(childID string) ([]models.Individual, error) {
+	relCollection := database.RelationsCollection()
+	indCollection := database.IndividualsCollection()
+	var parents []models.Individual
+
+	// Chercher les relations où cette personne est l'enfant
+	cursor, err := relCollection.Find(context.Background(), bson.M{"child_id": childID})
+	if err != nil {
+		return nil, err
+	}
+
+	var relations []models.Relation
+	err = cursor.All(context.Background(), &relations) // on cherche et onmet tout dans relations
+	if err != nil {
+		return nil, err
+	}
+
+	// Pour chaque relation, trouver le parent
+	for _, rel := range relations { // pour chaque relation
+		var parent models.Individual
+		err := indCollection.FindOne(context.Background(), bson.M{"_id": rel.ParentID}).Decode(&parent)
+		// cherche la personne avec cet ID celui quon avait dans relations donc chaque une relation
+		if err == nil {
+			parents = append(parents, parent) //ajoute le parent à la liste
+		}
+	}
+
+	return parents, nil
+}
+
+// Trouver les enfants d'une personne
+func GetChildren(parentID string) ([]models.Individual, error) {
+	relCollection := database.RelationsCollection()
+	indCollection := database.IndividualsCollection()
+	var children []models.Individual
+
+	// Chercher les relations où cette personne est le parent
+	cursor, err := relCollection.Find(context.Background(), bson.M{"parent_id": parentID})
+	if err != nil {
+		return nil, err
+	}
+
+	var relations []models.Relation
+	err = cursor.All(context.Background(), &relations)
+	if err != nil {
+		return nil, err
+	}
+
+	// Pour chaque relation, trouver l'enfant
+	for _, rel := range relations {
+		var child models.Individual
+		err := indCollection.FindOne(context.Background(), bson.M{"_id": rel.ChildID})
+		if err == nil {
+			children = append(children, child)
+		}
+	}
+
+	return children, nil
+}
